90380462e8523be3c086d9efd21a6f1ad925a811
TomasPetricek <45512445+tomaspetricek@users.noreply.github.com>
2023-08-15 18:47:48 +0200

    Medium stone game

--- medium/stone_game.hpp
+++ medium/stone_game.hpp
@@ -8,13 +8,38 @@
 #include <vector>
 #include <cassert>
 #include <iostream>
+#include <algorithm>
+#include <numeric>
 
 // topic: dynamic programming
 // link: https://leetcode.com/problems/stone-game/
 namespace leet_code {
+    constexpr int empty = -1;
+
+    int alice_max_total(int f, int l, std::vector<int>& piles,
+            std::vector<std::vector<int>>& cache)
+    {
+        if (f>l) {
+            return 0;
+        }
+        if (cache[f][l]==empty) {
+            int left{0}, right{0};
+            // check alice turn
+            if ((l-f)%2) {
+                left = piles[f];
+                right = piles[l];
+            }
+            cache[f][l] = std::max(alice_max_total(f+1, l, piles, cache)+left,
+                    alice_max_total(f, l-1, piles, cache)+right);
+        }
+        return cache[f][l];
+    }
+
     bool stoneGame(std::vector<int>& piles)
     {
-        return true;
+        std::vector<std::vector<int>> cache(piles.size(), std::vector<int>(piles.size(), empty));
+        int bob_max = std::accumulate(piles.begin(), piles.end(), 0)/2;
+        return alice_max_total(0, static_cast<int>(piles.size()-1), piles, cache)>bob_max;
     }
 
     struct sample {
