ae01552ccf9caedd0538b367875b55cb334cdc9d
Tomáš Petříček <tomaspetricek@Tomas--Air.home>
2024-06-27 12:31:37 +0200

    easy: kids with greatest number of candies

---
+++ easy/kids_with_greatest_number_of_candies.hpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <cassert>
+#include <array>
+#include <algorithm>
+
+namespace leet_code {
+    std::vector<bool> kidsWithCandies(std::vector<int>& candies, int extraCandies) {
+        std::vector<bool> result;
+        result.reserve(candies.size());
+        int max_count{candies[0]};
+
+        for (const auto& count: candies) {
+            max_count = std::max(count, max_count);
+        }
+        for (const auto& count: candies) {
+            result.emplace_back((count+extraCandies)>=max_count);
+        }
+        return result;
+    }
+}
