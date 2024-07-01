da1cf31e18b24dfa55136eeb50a2c9107c03c282
Tomáš Petříček <tomaspetricek@Tomas--MacBook-Air.local>
2024-07-01 21:29:51 +0200

    easy: contains duplicate ii

---
+++ easy/contains_duplicate_ii.hpp
@@ -0,0 +1,22 @@
+#include <unordered_map>
+#include <vector>
+
+namespace leet_code
+{
+    bool containsNearbyDuplicate(std::vector<int> &nums, int k)
+    {
+        std::unordered_map<int, int> occurrences;
+
+        for (std::size_t curr{0}; curr < nums.size(); ++curr)
+        {
+            auto& prev = occurrences[nums[curr]];
+
+            if (prev && ((curr - (prev - 1)) <= k))
+            {
+                return true;
+            }
+            prev = curr + 1;
+        }
+        return false;
+    }
+} // namespace leet_code
